89da06e2c6b4515fc89a83b77617529e2a537a85
Justin C. Miller <justin@devjustinian.com>
2024-09-29 18:47:38 -0700

    Convert to discord's slash command interation API

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/justinian/discordice
 go 1.13
 
 require (
-	github.com/bwmarrin/discordgo v0.20.2
-	github.com/justinian/dice v1.0.0
+	github.com/bwmarrin/discordgo v0.28.1
+	github.com/justinian/dice v1.0.3
 	github.com/kelseyhightower/envconfig v1.4.0
 )
--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"os/signal"
 	"strings"
-	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/justinian/dice"
@@ -14,154 +13,115 @@ import (
 )
 
 type Config struct {
-	Token string
+	GuildID        string // Test guild ID
+	Token          string // Bot API token
+	RemoveCommands bool   // Remove commands on shutdown
 }
 
-var helpMsg = strings.Join([]string{
-	"Hi! I'm a dice bot.",
-	"Use `!roll <format>` to roll some dice.",
-	"For format help, see: http://bit.ly/dice-bot",
-}, "\n")
+func rollHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	// Access options in the order provided by the user.
+	options := i.ApplicationCommandData().Options
+	optionStrs := make([]string, len(options))
+	for i := range options {
+		optionStrs[i] = options[i].StringValue()
+	}
+
+	result, reason, err := dice.Roll(strings.Join(optionStrs, " "))
+	if err != nil {
+		errMsg := fmt.Sprintf(":x: *Dice error:* %s", err)
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: errMsg,
+				Flags:   discordgo.MessageFlagsEphemeral,
+			},
+		})
+		return
+	}
+
+	if reason != "" {
+		reason = fmt.Sprintf(" *%s*", reason)
+	}
+
+	resultStrs := strings.Split(result.String(), "\n")
+	for i, s := range resultStrs {
+		if i == 0 {
+			resultStrs[i] = fmt.Sprintf("*%s*", strings.TrimSpace(s))
+		} else {
+			resultStrs[i] = fmt.Sprintf("_%s_", strings.TrimSpace(s))
+		}
+	}
+
+	text := fmt.Sprintf("Rolling `%s`:%s\n%s",
+		result.Description(),
+		reason,
+		strings.Join(resultStrs, "\n"))
+
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		// Ignore type for now, they will be discussed in "responses"
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{Content: text},
+	})
+}
 
 func main() {
 	var c Config
 	envconfig.Process("discordice", &c)
 
-	if c.Token == "" {
-		log.Fatal("No token provided.")
-	}
-
-	// Create a new Discord session using the provided bot token.
-	dg, err := discordgo.New("Bot " + c.Token)
+	s, err := discordgo.New("Bot " + c.Token)
 	if err != nil {
-		log.Fatalf("Error creating Discord session: ", err)
-		return
+		log.Fatalf("Invalid bot parameters: %v", err)
 	}
 
-	// Register ready as a callback for the ready events.
-	dg.AddHandler(ready)
+	var rollCommand = discordgo.ApplicationCommand{
+		Name:        "roll",
+		Description: "Roll some dice!",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "expression",
+				Description: "Dice expression to roll with optional description (e.g. '2d6 damage')",
+				Required:    true,
+			},
+		},
+	}
 
-	// Register messageCreate as a callback for the messageCreate events.
-	dg.AddHandler(messageCreate)
+	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		commandName := i.ApplicationCommandData().Name
+		if commandName == "roll" {
+			rollHandler(s, i)
+		}
+	})
 
-	// Register guildCreate as a callback for the guildCreate events.
-	dg.AddHandler(guildCreate)
+	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
+	})
 
-	// Open the websocket and begin listening.
-	err = dg.Open()
+	err = s.Open()
 	if err != nil {
-		log.Fatalf("Error opening Discord session: %s", err)
+		log.Fatalf("Cannot open the session: %v", err)
 	}
 
-	// Wait here until CTRL-C or other term signal is received.
-	log.Print("Discordice is now running.  Press CTRL-C to exit.")
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-	<-sc
-
-	// Cleanly close down the Discord session.
-	dg.Close()
-}
-
-// This function will be called (due to AddHandler above) when the bot receives
-// the "ready" event from Discord.
-func ready(s *discordgo.Session, event *discordgo.Ready) {
-	log.Print("Discord session ready.")
-}
-
-// This function will be called (due to AddHandler above) every time a new
-// message is created on any channel that the autenticated bot has access to.
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
-	if m.Author.ID == s.State.User.ID {
-		return
+	log.Println("Adding command...")
+	registeredCmd, err := s.ApplicationCommandCreate(s.State.User.ID, c.GuildID, &rollCommand)
+	if err != nil {
+		log.Panicf("Cannot create roll command: %v", err)
 	}
 
-	if strings.HasPrefix(m.Content, "!rollhelp") {
-		c, err := s.UserChannelCreate(m.Author.ID)
+	defer s.Close()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	log.Println("Press Ctrl+C to exit")
+	<-stop
+
+	if c.RemoveCommands {
+		log.Println("Removing command...")
+		err := s.ApplicationCommandDelete(s.State.User.ID, c.GuildID, registeredCmd.ID)
 		if err != nil {
-			log.Printf("Couldn't open DM to user: %v", err)
-			return
-		}
-
-		_, err = s.ChannelMessageSend(c.ID, helpMsg)
-		if err != nil {
-			log.Printf("Error sending message to %v: %s", c, err)
-			return
-		}
-
-		mc, err := s.State.Channel(m.ChannelID)
-		if err == nil && mc.Type == discordgo.ChannelTypeGuildText {
-			err = s.ChannelMessageDelete(m.ChannelID, m.ID)
-			if err != nil {
-				log.Printf("Error deleting message in %v: %s", mc, err)
-				return
-			}
-		}
-	} else if strings.HasPrefix(m.Content, "!roll") {
-		// Find the channel that the message came from.
-		c, err := s.State.Channel(m.ChannelID)
-		if err != nil {
-			// Could not find channel.
-			log.Printf("No channel ID: %v", err)
-			return
-		}
-
-		result, reason, err := dice.Roll(m.Content)
-		if err != nil {
-			log.Printf("Dice error: %s", err)
-			return
-		}
-
-		if reason != "" {
-			reason = fmt.Sprintf(" *%s*", reason)
-		}
-
-		resultStrs := strings.Split(result.String(), "\n")
-		for i, s := range resultStrs {
-			if i == 0 {
-				resultStrs[i] = fmt.Sprintf("*%s*", strings.TrimSpace(s))
-			} else {
-				resultStrs[i] = fmt.Sprintf("_%s_", strings.TrimSpace(s))
-			}
-		}
-
-		text := fmt.Sprintf("*<@%s>* rolled `%s`:%s\n%s",
-			m.Author.ID,
-			result.Description(),
-			reason,
-			strings.Join(resultStrs, "\n"))
-
-		_, err = s.ChannelMessageSend(m.ChannelID, text)
-		if err != nil {
-			log.Printf("Error sending message to %v: %s", c, err)
-			return
-		}
-
-		if c.Type == discordgo.ChannelTypeGuildText {
-			err = s.ChannelMessageDelete(m.ChannelID, m.ID)
-			if err != nil {
-				log.Printf("Error deleting message in %v: %s", c, err)
-				return
-			}
+			log.Panicf("Cannot delete roll command: %v", err)
 		}
 	}
-}
 
-// This function will be called (due to AddHandler above) every time a new
-// guild is joined.
-func guildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
-	if event.Guild.Unavailable {
-		return
-	}
-
-	log.Printf("Joined Guild: %s (%s) in %s",
-		event.Guild.Name, event.Guild.ID, event.Guild.Region)
-
-	owner, err := s.User(event.Guild.OwnerID)
-	if err == nil {
-		log.Printf("\tOwned by: %s#%s (%s)",
-			owner.Username, owner.Discriminator, owner.ID)
-	}
+	log.Println("Gracefully shutting down.")
 }
